5f97a287f99de593ddc002b274d5fd7b457c5ae4
Ricky Vang <vangcheemeng@yahoo.com>
2024-10-11 10:42:06 -0600

    fixed css in util - indexjs file

--- utilities/index.js
+++ utilities/index.js
@@ -6,7 +6,7 @@ const Util = {}
  ************************** */
 Util.getNav = async function (req, res, next) {
   let data = await invModel.getClassifications()
-  let list = "<ul>"
+  let list = "<ul class='nav-list'>"
   list += '<li><a href="/" title="Home page">Home</a></li>'
   data.rows.forEach((row) => {
     list += "<li>"
