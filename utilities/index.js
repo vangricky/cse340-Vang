6d79da97046702760696d31a6d5e2fc7b907a7f5
Ricky Vang <vangcheemeng@yahoo.com>
2024-10-19 15:36:28 -0600

    added styles to individual cars

--- utilities/index.js
+++ utilities/index.js
@@ -60,4 +60,12 @@ Util.buildClassificationGrid = async function(data){
 
 
 
+/*****************************************
+* Middleware For Handling Errors
+* Wrap other functions in this for
+* General Error Handling
+****************************************/
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
+
+
 module.exports = Util
