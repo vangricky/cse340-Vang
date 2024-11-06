7c07fb0362c4f4b847b21c02e1129c4c7ffc58a8
Ricky Vang <vangcheemeng@yahoo.com>
2024-11-06 13:18:12 -0700

    validattions server side

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require("../utilities/account-validation")
 
 //login view page
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
@@ -9,7 +10,18 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 //register post req
-router.post("/register", utilities.handleErrors(accountController.registerAccount))
+router.post("/register",
+regValidate.registrationRules(),
+regValidate.checkRegData,
+ utilities.handleErrors(accountController.registerAccount))
+
+ // Process the login attempt
+router.post(
+    "/login",
+    (req, res) => {
+      res.status(200).send('login process')
+    }
+  )
 
 
 module.exports = router;
